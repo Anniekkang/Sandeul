2e6038d347693a396be43a047457a410f9c879bc
annie <knr9144@icloud.com>
2022-10-04 15:56:34 +0900

    10.4 - 위치제공서비스 권한 설정 done

--- Mountaing/FirstTap/CurrentLocation.swift
+++ Mountaing/FirstTap/CurrentLocation.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 import CoreLocation
-
+import UIKit
+import Toast
 
 extension FirstViewController : CLLocationManagerDelegate {
     
@@ -38,9 +39,87 @@ extension FirstViewController : CLLocationManagerDelegate {
             
             
         }
+        locationManger.stopUpdatingLocation()
     }
 
+    //
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+    }
+    
+    func checkAuthorization() {
+        let authorizationStatus : CLAuthorizationStatus = locationManger.authorizationStatus
+        
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserAuthorization(authorizationStatus)
+        } else {
+            "위치서비스 꺼져있음"
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkAuthorization()
+        
+    }
 
+    
+    func checkUserAuthorization (_ status : CLAuthorizationStatus){
+        
+        switch status {
+        case .authorizedWhenInUse :
+            locationManger.stopUpdatingLocation()
+            print("When In Use")
+        case .denied, .restricted :
+            print("아이폰 설정 유도")
+            showRequestAlert()
+        case .notDetermined :
+            print("Not Determined")
+            locationManger.desiredAccuracy = kCLLocationAccuracyBest
+            locationManger.requestWhenInUseAuthorization()
+        default :
+            print("nothing")
+        }
+     
+    }
+    
+    func showRequestAlert(){
+        
+        let requestAlert = UIAlertController(title: "위치정보 이용", message: "'근처의 산' 서비스를 이용할 수 없습니다.기기의 '설정 > 개인정보보호'에서 위치서비스를 켜주세요 ", preferredStyle: .alert)
+        let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _
+            in
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
+            }
+            
+        }
+        
+        let cancel = UIAlertAction(title: "서비스 이용 안함", style: .default) { _ in
+            var style = ToastStyle()
+            style.backgroundColor = colorCustom.shared.greenColor
+            style.messageColor = colorCustom.shared.creamBackgroundColor
+            style.messageAlignment = .center
+            style.titleAlignment = .center
+            style.fadeDuration = 0.8
+            
+            
+            self.mainView.makeToast("'같은 지역에 있는 산'은 \n '서울특별시' 기준으로 보여집니다", duration: 1.5,
+                                    point: CGPoint(x: self.mainView.center.x, y: 250),
+                                   title: "위치서비스 거부",
+                                   image: nil,
+                                    style: style,
+                                   completion: nil)
+            
+            
+        }
+        
+        requestAlert.addAction(goSetting)
+        requestAlert.addAction(cancel)
+        
+        present(requestAlert, animated: true)
+        
+        
+    }
 
-
+    
 }
